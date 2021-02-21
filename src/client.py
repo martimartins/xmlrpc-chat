f70a0e1e8d5cae470a26dadc19f618d5074c479e
Martim Martins <67507845+jokeman332@users.noreply.github.com>
2021-02-21 10:54:19 +0000

    Update client.py

--- src/client.py
+++ src/client.py
@@ -31,14 +31,14 @@ def check_messagens(proxy):
             for d in data:
                 if d not in temp_messages:
                     temp_messages.append(d)
-                    print(f"\n{d[int(len(d)-4:)]}")
+                    print(f"\n{d[:int(len(d)-4)]}")
 
 def pergunta_por_message():
     while True:
         msg = input()
         if msg != "":
             id_of_message = random.randint(1000, 9999)
-            messagens_totais = proxy.send_message(msg+id_of_message, nome)
+            messagens_totais = proxy.send_message(msg+str(id_of_message), nome)
 
 def main():
     global nome
